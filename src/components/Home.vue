3370e2990af919692b36a008c9c6391ea779df86
Annisa Suci Soleha <annisasucisoleha5@gmail.com>
2025-12-01 18:42:20 +0700

    ADD: gambar terbaru

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,16 +15,16 @@ import ProductGrid from '../components/ProductGrid.vue'
 import Testimonials from '../components/Testimonials.vue'
 
 const newArrivals = [
-  { id: 1, name: 'Koko Pria', price: 'Rp250.000', discount: 'New In', image: 'https://images.unsplash.com/photo-1622470953794-aa9c70b0fb9d?w=400' },
-  { id: 2, name: 'Gamis Wanita', price: 'Rp280.000', discount: 'New In', image: 'https://images.unsplash.com/photo-1583486932668-31780f1f5527?w=400' },
-  { id: 3, name: 'Koko Pria', price: 'Rp290.000', discount: 'New In', image: 'https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=400' },
-  { id: 4, name: 'Koko Pria', price: 'Rp200.000', discount: 'New In', image: 'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?w=400' },
+  { id: 1, name: 'Koko Pria', price: 'Rp250.000', discount: 'New In', image: '/koko-putih.png' },
+  { id: 2, name: 'Gamis Wanita', price: 'Rp280.000', discount: 'New In', image: '/gamis-pink.png' },
+  { id: 3, name: 'Koko Pria', price: 'Rp290.000', discount: 'New In', image: '/koko-abu.png' },
+  { id: 4, name: 'Koko Pria', price: 'Rp200.000', discount: 'New In', image: '/gamis-coklat.png' },
 ]
 
 const bestSellers = [
-  { id: 5, name: 'Koko Pria', price: 'Rp200.000', image: 'https://images.unsplash.com/photo-1622470953794-aa9c70b0fb9d?w=400' },
-  { id: 6, name: 'Gamis Wanita', price: 'Rp200.000', image: 'https://images.unsplash.com/photo-1583486932668-31780f1f5527?w=400' },
-  { id: 7, name: 'Koko Pria', price: 'Rp290.000', image: 'https://images.unsplash.com/photo-1578632292335-df3abbb0d586?w=400' },
-  { id: 8, name: 'Koko Pria', price: 'Rp200.000', image: 'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?w=400' },
+  { id: 5, name: 'Koko Pria Casual', price: 'Rp220.000', image: '/koko-hijau.png' },
+  { id: 6, name: 'Abaya Wanita Syari', price: 'Rp300.000', image: '/abaya-hitam.png'  },
+  { id: 7, name: 'Gamis Executive', price: 'Rp350.000', image: '/gamis-p.png' },
+  { id: 8, name: 'Hijab Trendy', price: 'Rp240.000', image: '/hijab.png' },
 ]
 </script>
